a5e7152d215b69076f64c8a3cd23235dc0068efe
Vinicius de Souza Santos <desouza850@gmail.com>
2025-03-26 14:02:56 -0300

    criação de componentes login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { UpdateComponent } from './components/update/update.component';
+import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
     {path: 'easy-task', component: TasksComponent},
     {path: '', component: TasksComponent},
-    {path: 'update/:id', component: UpdateComponent}
+    {path: 'update/:id', component: UpdateComponent},
+    {path: 'login', component: LoginComponent}
 ];
