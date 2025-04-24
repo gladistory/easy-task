5443fd28a7f5746bad2de23b290fbe053feda0bc
Vinicius de Souza Santos <desouza850@gmail.com>
2025-04-24 10:49:42 -0300

    resolvendo bug logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -15,7 +15,7 @@ export class HeaderComponent {
 
   logout(): void {
     this.authService.logout();
-    this.router.navigate(['/login']);
+    this.router.navigate(['/']);
   }
   
 }
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -7,7 +7,7 @@ import { Task } from '../../../Task';
 })
 export class TasksService {
 
-  private apiUrl = 'http://localhost:3002/api/tasks';
+  private apiUrl = 'http://localhost:3001/api/tasks';
 
   constructor(private http: HttpClient) {}
 
